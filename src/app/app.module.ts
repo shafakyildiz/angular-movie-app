e835059138afb6f230e74dee9db1211c09252cad
shafakyildiz <shafakyildiz@gmail.com>
2021-11-03 14:19:10 +0300

    added angular material and angular gauge dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { GaugeModule } from 'angular-gauge';
+import  {MatTabsModule} from '@angular/material/tabs'
 
 @NgModule({
   declarations: [
